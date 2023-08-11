87a5c11acad2d5edf7824a37d096dfdf75bd54a8
Jegan <jeganeeemkce@gmail.com>
2023-08-11 10:48:26 +0530

    login removed

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const { authenticateToken } = require("../Pageconstants/authverify");
 const { mailfiltercontroller } = require("../controller/Mailfiltercontroller");
 const router = express.Router();
 
-router.post("/mail", authenticateToken, usermaildata.maildata);
-router.get("/mail", authenticateToken, usermaildata.getmaildata);
-router.get("/getmail", authenticateToken, usermaildata.getbyidcontroller);
-router.delete("/deletemail", authenticateToken,usermaildata.deletemail);
-router.put("/updatestatus",authenticateToken,usermaildata.putid);
-router.get("/search",authenticateToken,mailfiltercontroller.filter)
+router.post("/mail", usermaildata.maildata);
+router.get("/mail", usermaildata.getmaildata);
+router.get("/getmail", usermaildata.getbyidcontroller);
+router.delete("/deletemail",usermaildata.deletemail);
+router.put("/updatestatus",usermaildata.putid);
+router.get("/search",mailfiltercontroller.filter)
 module.exports = router;
